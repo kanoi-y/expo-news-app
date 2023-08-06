feb37b28bca18df113d0a456895dee83b94bea51
kanoi-y <furafuratonbo@gmail.com>
2023-08-06 22:27:41 +0900

    ✨ ListItemコンポーネントを作成

--- App.js
+++ App.js
@@ -1,24 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { ListItem } from "./components/ListItem";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.itemContainer}>
-        <View style={styles.leftContainer}>
-          <Image
-            style={{ width: 100, height: 100 }}
-            source={{ uri: "https://picsum.photos/300/300" }}
-          />
-        </View>
-        <View style={styles.rightContainer}>
-          <Text numberOfLines={3} style={styles.text}>
-            Type Safe, Extensible, and Modular by design. Forget you are even using a store. Type
-            Safe, Extensible, and Modular by design. Forget you are even using a store.
-          </Text>
-          <Text style={styles.subText}>React native</Text>
-        </View>
-      </View>
+      <ListItem />
+      <ListItem />
+      <ListItem />
       <StatusBar style="auto" />
     </View>
   );
@@ -30,26 +19,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#eee",
     alignItems: "center",
     justifyContent: "center",
-  },
-  itemContainer: {
-    height: 100,
-    width: "100%",
-    flexDirection: "row",
-    backgroundColor: "#fff",
-  },
-  leftContainer: {
-    width: 100,
-  },
-  rightContainer: {
-    flex: 1,
-    padding: 10,
-    justifyContent: "space-between",
-  },
-  text: {
-    fontSize: 16,
-  },
-  subText: {
-    fontSize: 12,
-    color: "gray",
+    gap: 5,
   },
 });
