887f4386bdac6b4ec8de19498585ad666e296cc1
kanoi-y <furafuratonbo@gmail.com>
2023-09-02 23:16:09 +0900

    ✨ newsapiを使用してニュースを取得して表示するように実装

--- App.js
+++ App.js
@@ -2,13 +2,21 @@ import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
 import { FlatList, SafeAreaView, StyleSheet } from "react-native";
 import { ListItem } from "./components/ListItem";
-import dummyArticles from "./dummies/articles";
+import axios from "axios";
+import { NEWS_API_KEY } from "@env";
+
+const URL = `https://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${NEWS_API_KEY}`
 
 export default function App() {
   const [articles, setArticles] = useState([]);
 
-  const fetchArticles = () => {
-    setArticles(dummyArticles);
+  const fetchArticles = async () => {
+    try {
+      const res = await axios.get(URL)
+      setArticles(res.data.articles)
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   useEffect(() => {
