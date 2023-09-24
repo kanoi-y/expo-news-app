82aeb023355d20dc85f47d3cbdf496508432a522
kanoi-y <furafuratonbo@gmail.com>
2023-09-24 23:54:10 +0900

    ✨ ReactNavigationを導入

--- App.js
+++ App.js
@@ -1,44 +1,14 @@
-import { StatusBar } from "expo-status-bar";
-import { useEffect, useState } from "react";
-import { FlatList, SafeAreaView, StyleSheet } from "react-native";
-import { ListItem } from "./components/ListItem";
-import axios from "axios";
-import { NEWS_API_KEY } from "@env";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { HomeScreen } from "./screens/HomeScreens";
 
-const URL = `https://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${NEWS_API_KEY}`
-
+const Stack = createNativeStackNavigator();
 export default function App() {
-  const [articles, setArticles] = useState([]);
-
-  const fetchArticles = async () => {
-    try {
-      const res = await axios.get(URL)
-      setArticles(res.data.articles)
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchArticles();
-  }, []);
   return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={articles}
-        renderItem={({ item }) => (
-          <ListItem imageUrl={item.urlToImage} title={item.title} author={item.author} />
-        )}
-        keyExtractor={(_item, index) => index.toString()}
-      />
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false}} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#eee",
-  },
-});
---
+++ screens/HomeScreens.js
@@ -0,0 +1,42 @@
+import { NEWS_API_KEY } from "@env";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { FlatList, SafeAreaView, StyleSheet } from "react-native";
+import { ListItem } from "../components/ListItem";
+
+const URL = `https://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${NEWS_API_KEY}`;
+
+export const HomeScreen = () => {
+  const [articles, setArticles] = useState([]);
+
+  const fetchArticles = async () => {
+    try {
+      const res = await axios.get(URL);
+      setArticles(res.data.articles);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem imageUrl={item.urlToImage} title={item.title} author={item.author} />
+        )}
+        keyExtractor={(_item, index) => index.toString()}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#eee",
+  },
+});
