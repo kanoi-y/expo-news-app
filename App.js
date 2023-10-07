beb083884da736bfc1e2526358b872fbe27b596d
kanoi-y <furafuratonbo@gmail.com>
2023-10-07 16:08:10 +0900

    ✨ ボトムナビゲーションを実装

--- App.js
+++ App.js
@@ -1,16 +1,45 @@
+import { FontAwesome } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { HomeScreen } from "./screens/HomeScreens";
 import { ArticleScreen } from "./screens/ArticleScreen";
+import { ClipScreen } from "./screens/ClipScreen";
+import { HomeScreen } from "./screens/HomeScreen";
 
+const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
+
+const screenOptions = ({ route }) => ({
+  tabBarIcon: ({ focused, color, size }) => {
+    let iconName;
+    switch (route.name) {
+      case "HomeTab":
+        iconName = "home";
+        break;
+      case "ClipTab":
+        iconName = "bookmark";
+        break;
+    }
+    return <FontAwesome name={iconName} size={size} color={color} />;
+  },
+});
+
+const HomeStack = () => {
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="Article" component={ArticleScreen} />
+    </Stack.Navigator>
+  );
+};
+
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false}} />
-        <Stack.Screen name="Article" component={ArticleScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator screenOptions={screenOptions}>
+        <Tab.Screen name="HomeTab" component={HomeStack} options={{ headerShown: false, title: "Home" }} />
+        <Tab.Screen name="ClipTab" component={ClipScreen} options={{ headerShown: false, title: "Clip" }} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
