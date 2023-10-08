f9b5456c85924ebf55e15babc05aad2e0d0d1c67
kanoi-y <furafuratonbo@gmail.com>
2023-10-08 22:53:56 +0900

    ✨ クリップ機能を実装

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -1,11 +1,27 @@
 import { SafeAreaView, StyleSheet } from "react-native";
 import { WebView } from "react-native-webview";
+import { useDispatch, useSelector } from "react-redux";
+import { ClipButton } from "../components/ClipButton";
+import { addClip, deleteClip } from "../store/userSlice";
 
 export const ArticleScreen = ({ route }) => {
   const { article } = route.params;
 
+  const dispatch = useDispatch();
+  const clips = useSelector((state) => state.user.clips);
+  const isClipped = clips.some((clip) => clip.url === article.url);
+
+  const onPressClip = () => {
+    if (isClipped) {
+      dispatch(deleteClip(article));
+      return;
+    }
+    dispatch(addClip(article));
+  };
+
   return (
     <SafeAreaView style={styles.container}>
+      <ClipButton onPress={onPressClip} enabled={isClipped} />
       <WebView source={{ uri: article.url }} />
     </SafeAreaView>
   );
