ec8bbe5cd15d085f4a07722b2b7540428a3d39dc
kanoi-y <furafuratonbo@gmail.com>
2023-10-08 22:27:39 +0900

    ✨ userSliceを実装

--- App.js
+++ App.js
@@ -2,9 +2,11 @@ import { FontAwesome } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Provider } from "react-redux";
 import { ArticleScreen } from "./screens/ArticleScreen";
 import { ClipScreen } from "./screens/ClipScreen";
 import { HomeScreen } from "./screens/HomeScreen";
+import { store } from "./store";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -35,11 +37,21 @@ const HomeStack = () => {
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator screenOptions={screenOptions}>
-        <Tab.Screen name="HomeTab" component={HomeStack} options={{ headerShown: false, title: "Home" }} />
-        <Tab.Screen name="ClipTab" component={ClipScreen} options={{ headerShown: false, title: "Clip" }} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Tab.Navigator screenOptions={screenOptions}>
+          <Tab.Screen
+            name="HomeTab"
+            component={HomeStack}
+            options={{ headerShown: false, title: "Home" }}
+          />
+          <Tab.Screen
+            name="ClipTab"
+            component={ClipScreen}
+            options={{ headerShown: false, title: "Clip" }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
